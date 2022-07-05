9da17ec45cf04159db2e74583181de865b01d65a
Cem <cemkgrbz@gmail.com>
2022-07-05 14:21:17 +0200

    modified js file

---
+++ solution.js
@@ -0,0 +1,22 @@
+//1
+
+console.log(Math.min(-1, 4));
+console.log(Math.max(-1, 4));
+
+//2
+//a
+console.log(Math.ceil(3321.32321));
+console.log(Math.ceil(326.76));
+console.log(Math.ceil(76788.7));
+console.log(Math.ceil(-9.78));
+console.log(Math.ceil(43.342));
+//b
+console.log(Math.floor(3321.32321));
+console.log(Math.floor(326.76));
+console.log(Math.floor(76788.7));
+console.log(Math.floor(-9.78));
+console.log(Math.floor(28.329));
+
+//3
+
+console.log(Math.ceil(Math.random() * 6));
